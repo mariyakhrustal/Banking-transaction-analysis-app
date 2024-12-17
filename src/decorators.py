0f9a03a59ff0df339b0f61ac32e3dc0e086d6a48
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-12-17 16:59:00 +0500

    reports and decorators fixes

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,19 @@
+import logging
 import os
 from functools import wraps
 from typing import Any, Callable, Optional
+
+
+logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)  # pragma: no cover
+
+logger = logging.getLogger("decorators")
+file_handler = logging.FileHandler(os.path.join(logs_dir, "decorators.log"), mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def report(filename: Optional[str] = None) -> Callable:
@@ -8,15 +21,19 @@ def report(filename: Optional[str] = None) -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
+            logger.info("Определение файла для декоратора")
             report_dir = "../reports"
             if filename:
+                logger.info("Работа с переданным файлом для записи")
                 report_dir = os.path.dirname(filename)
                 if report_dir and not os.path.exists(report_dir):
                     os.makedirs(report_dir)
             else:
+                logger.info("Работа с дефолтным файлом для записи")
                 if not os.path.exists(report_dir):
                     os.makedirs(report_dir)
             try:
+                logger.info("Вызов функции внутри декоратора")
                 result = func(*args, **kwargs)
                 report_message = f"Результат функции {func.__name__}: {result}\n"
                 if filename:
@@ -25,8 +42,10 @@ def report(filename: Optional[str] = None) -> Callable:
                 else:
                     with open(os.path.join(report_dir, "report.txt"), "w", encoding="utf-8") as file:
                         file.write(report_message)
+                logger.info("Запись в файл результата успешной работы функции")
                 return result
             except Exception as error:
+                logger.info("Запись в файл об ошибке работы функции")
                 report_message = f"{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}\n"
                 if filename:
                     with open(filename, "w", encoding="utf-8") as file:
@@ -34,5 +53,6 @@ def report(filename: Optional[str] = None) -> Callable:
                 else:
                     with open(os.path.join(report_dir, "report.txt"), "w", encoding="utf-8") as file:
                         file.write(report_message)
+                logger.info("Завершение работы декоратора")
         return wrapper
     return decorator
--- src/reports.py
+++ src/reports.py
@@ -1,8 +1,57 @@
+import json
+import logging
+import os
+from datetime import datetime, timedelta
 from typing import Optional
 
 import pandas as pd
 
+from src.decorators import report
+from src.utils import read_file_xlsx
 
-def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)  # pragma: no cover
+
+logger = logging.getLogger("reports")
+file_handler = logging.FileHandler(os.path.join(logs_dir, "reports.log"), mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+@report("../reports/report.txt")
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> str:
     """Функция возвращает траты по заданной категории за последние три месяца (от переданной даты)"""
-    pass
+    logger.info("Начало работы функции траты по категориям")
+    category = category.capitalize()
+    if date:
+        input_date = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
+    else:
+        input_date = datetime.now()
+    logger.info("Определение времени для работы с транзакциями")
+    transactions["Дата операции"] = transactions["Дата операции"].apply(
+        lambda x: datetime.strptime(x, "%d.%m.%Y %H:%M:%S") if pd.notnull(x) else None
+    )
+    three_month_ago = input_date - timedelta(days=90)
+    filtered_df = transactions[
+        (transactions["Категория"] == category) &
+        (transactions["Дата операции"] >= three_month_ago) &
+        (transactions["Дата операции"] <= input_date) &
+        (transactions["Сумма платежа"] < 0)
+        ]
+    logger.info("Транзакции переводятся в список словарей для дальнейшей работы")
+    transacts_dict = filtered_df.to_dict(orient="records")
+    final_list = []
+    for item in transacts_dict:
+        item["Дата операции"] = item["Дата операции"].strftime("%d.%m.%Y %H:%M:%S")
+        final_list.append(item)
+    logger.info(f"Формирование итогового списка транзакций всего: {len(final_list)} транзакций")
+    logger.info("Завершение работы функции. Формирование итоговой json строки")
+    return json.dumps(final_list, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":  # Пример использования
+    df = read_file_xlsx("../data/operations.xlsx")
+    print(spending_by_category(df, "медицина", "11.12.2021 00:00:00"))
